b49a84fec4ad11c03021feac7e07e9af5c4133c5
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-09-05 18:59:22 +0600

    chore: Add react-native-maps and react-native-maps-directions dependencies

---
+++ components/Map.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+
+export default function Map() {
+  return (
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
+      <Text>Map</Text>
+    </MapView>
+  );
+}
